1843ac5108362adce3a7bc44bf3fba8ca5a6bb2b
Dave Nicholson <fatnic388@gmail.com>
2016-06-19 21:02:24 +0100

    can check if a point is in the rect

--- rect.js
+++ rect.js
@@ -4,25 +4,53 @@ function Rect(position, size) {
 
     Object.defineProperty(this, "x", {
         get: function() { return this.position.x; },
-        set: function(x) { return this.position.x = x; }
+        set: function(x) { this.position.x = x; }
     });
 
     Object.defineProperty(this, "y", {
         get: function() { return this.position.y; },
-        set: function(y) { return this.position.y = y; }
+        set: function(y) { this.position.y = y; }
     });
 
     Object.defineProperty(this, "width", {
         get: function() { return this.size.x; },
-        set: function(width) { return this.size.x = width; }
+        set: function(width) { this.size.x = width; }
     });
 
     Object.defineProperty(this, "height", {
         get: function() { return this.size.y; },
-        set: function(height) { return this.size.y = height; }
+        set: function(height) { this.size.y = height; }
+    });
+
+    Object.defineProperty(this, "points", {
+        get: function() {
+            return [
+                new Vec2(this.position.x, this.position.y),
+                new Vec2(this.position.x + this.size.x, this.position.y),
+                new Vec2(this.position.x + this.size.x, this.position.y + this.size.y),
+                new Vec2(this.position.x, this.position.y + this.size.y),
+            ];
+        }
     });
 }
 
-Rect.prototype.draw = function (context) {
-    Draw.rect(context, this.position, this.size);
+Rect.prototype.contains = function (point) {
+    if (this.position.x <= point.x && point.x <= this.position.x + this.size.x &&
+        this.position.y <= point.y && point.y <= this.position.y + this.size.y) {
+            return true;
+        }
+    return false;
+};
+
+Rect.prototype.intersects = function (rect2) {
+
+};
+
+Rect.prototype.draw = function (context, filled) {
+    var f =  (typeof filled === 'undefined') ? false : filled;
+    if (f) {
+        Draw.fillRect(context, this.position, this.size);
+    } else {
+        Draw.rect(context, this.position, this.size);
+    }
 };
