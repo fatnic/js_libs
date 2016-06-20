c150a95c5bcfc6e79463b90cb26d284190acfe1f
Dave Nicholson <fatnic388@gmail.com>
2016-06-20 19:42:24 +0100

    new segments file

--- rect.js
+++ rect.js
@@ -1,12 +1,3 @@
-function Segment(a, b) {
-    this.a = a;
-    this.b = b;
-
-    Object.defineProperty(this, "length", {
-        get: function() { return this.a.distanceTo(b); }
-    });
-}
-
 function Rect(position, size) {
     this.position = position;
     this.size = size;
