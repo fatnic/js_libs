6c5d2fbaad562bb5e91cfdaefa45cd83fa23d2fd
Dave Nicholson <fatnic388@gmail.com>
2016-06-26 19:54:02 +0100

    sorted out game delta n shit

--- game.js
+++ game.js
@@ -1,7 +1,13 @@
 function Game() {
     this.canvas = document.getElementById('canvas');
     this.ctx = canvas.getContext('2d');
-    
+
+    this.now = null;
+    this.delta = 0;
+    this.last = Tools.timestamp();
+    this.fps = 60;
+    this.step = 1/this.fps;
+
     this.gameStates = [];
 }
 
@@ -25,6 +31,12 @@ Game.prototype.peekState = function () {
 
 Game.prototype.gameLoop = function () {
     if(this.peekState() === null) { return; }
-    this.peekState().update();
+
+    this.now = Tools.timestamp();
+    this.delta = this.delta + Math.min(1, (this.now - this.last) / 1000);
+    while (this.delta > this.step) {
+        this.delta = this.delta - this.step;
+        this.peekState().update(this.step);
+    }
     this.peekState().draw();
 };
